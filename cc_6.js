7a9e6a12899c19b044849b78796babaab0f7f999
Alexia Hall <alexiahall@usf.edu>
2026-03-01 22:34:11 -0500

    Added the "addEmployee and listEmployee" in the cc_6.js file

--- cc_6.js
+++ cc_6.js
@@ -8,25 +8,53 @@ class Employee {
  }
 }
 
+class Manager extends Employee {
+    constructor (name, department, level="Manager", teamSize) {
+        super (name, department,level)
+        this.teamSize = teamSize;
+    }
+describe() {
+    return `${this.name} | ${this.department} | ${this.level} | ${this.teamSize} |`
+}
+}
+
 const Employees = [
 
- new Employee ("Allison Jones", "Marketing"),
- new Employee ("Jennifer Davis", "Finance"),
- new Employee("Daniel Ham", "HR"),
- new Employee ("Brianna Doan", "Marketing"),
+ new Manager ("Allison Jones", "Marketing","Manager", 5),
+ new Employee ("Jennifer Davis", "Finance", " "),
+ new Manager("Daniel Ham", "HR","Manager", 9),
+ new Employee ("Brianna Doan", "Marketing","Associate"),
 
 ];
 for (let emp of Employees) {
 console.log(emp.describe())
 }
 
-
-class Manager extends Employee {
-    constructor (name, department, level="Manager", teamSize) {
-        super (name,department,level);
-        this.teamSize = teamSize;
+class Company {
+    constructor() {
+        this.Employees = [];
     }
-describe() {
-    return `${this.name} | ${this.department} | ${this.teamSize} |`
+    addEmployee(Employee) { 
+        if (Employee) {
+            this.Employees.push(Employee)
+            console.log(`Added employee to company`);
+        
+        }
+    }
+    listEmployees() {
+        console.log('--- Company Employees---');
+        this.Employees.forEach((emp,idx) => {
+            console.log (`${idx + 1 } ${emp.describe()}`);
+        });
+    }
 }
-}
+const myCompany = new Company ();
+const emp1 = new Employee ("ALice Burns", "HR")
+const emp2 = new Employee ("Tom Spicer", "IT")
+const mgr1 = new Manager ("Lexy Hall", "IT")
+myCompany.addEmployee(emp1);
+myCompany.addEmployee(emp2);
+myCompany.addEmployee(mgr1);
+
+myCompany.listEmployees();
+console.log(myCompany.Employees);
