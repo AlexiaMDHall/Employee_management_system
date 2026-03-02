360d3694f442e4a9ee491ea0bcce270b7d8e06cb
Alexia Hall <alexiahall@usf.edu>
2026-03-01 21:05:20 -0500

    Create a subclass called Manager in cc_6.js file

--- cc_6.js
+++ cc_6.js
@@ -3,8 +3,30 @@ class Employee {
         this.name = name;
         this.department = department;
     }
+    describe () {
+    return `${this.name} | ${this.department} |`
+ }
 }
 
-describe () {
-    return `${this.name} | ${this.department} |`
+const Employees = [
+
+ new Employee ("Allison Jones", "Marketing"),
+ new Employee ("Jennifer Davis", "Finance"),
+ new Employee("Daniel Ham", "HR"),
+ new Employee ("Brianna Doan", "Marketing"),
+
+];
+for (let emp of Employees) {
+console.log(emp.describe())
+}
+
+
+class Manager extends Employee {
+    constructor (name, department, level="Manager", teamSize) {
+        super (name,department,level);
+        this.teamSize = teamSize;
+    }
+describe() {
+    return `${this.name} | ${this.department} | ${this.teamSize} |`
+}
 }
